4ab99751e67af22aca56d22b901717fd1e40b756
Wen Ow <wow@bc.beast-code.com>
2021-03-07 10:46:38 -0600

    Presentational and Container Components

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -49,10 +49,12 @@ class CampsiteInfoComponent extends Component {
   render() {
     if (this.props.campsite) {
       return (
-        <div className="row">
-          {this.renderCampsite(this.props.campsite)}
-          {this.renderComments(this.props.campsite.comments)}
-        </div>
+          <div className="container">
+            <div className="row">
+              {this.renderCampsite(this.props.campsite)}
+              {this.renderComments(this.props.campsite.comments)}
+            </div>
+          </div> 
       );
     } else {
       return <div>No campsite selected yet</div>;
