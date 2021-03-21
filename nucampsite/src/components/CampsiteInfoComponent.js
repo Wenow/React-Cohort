a28aa77e8833e78e9f4d572f3525c338fd935434
Wen Ow <wow@bc.beast-code.com>
2021-03-21 09:27:57 -0500

    Redux Actions

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -27,7 +27,7 @@ function RenderCampsite({ campsite }) {
   );
 }
 
-function RenderComments({ comments }) {
+function RenderComments({comments, addComment, campsiteId}) {
   console.log(comments);
   if (comments) {
     // comments !== null
@@ -47,7 +47,7 @@ function RenderComments({ comments }) {
             </div>
           );
         })}
-        <CommentForm />
+        <CommentForm campsiteId={campsiteId} addComment={addComment} />
       </div>
     );
   } else {
@@ -74,10 +74,10 @@ class CommentForm extends Component {
     });
   };
 
-  handleSubmit = (values) => {
+  handleSubmit(values) {
     this.toggleModal();
-    console.log(values);
-  };
+    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+  }
 
   render() {
     return (
@@ -174,7 +174,11 @@ function CampsiteInfo(props) {
         </div>
         <div className="row">
           <RenderCampsite campsite={props.campsite} />
-          <RenderComments comments={props.comments} />
+          <RenderComments 
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
         </div>
       </div>
     );
